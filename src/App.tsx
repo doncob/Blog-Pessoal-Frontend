65087c82474e08141be4e74af5c91819f62ba7ed
rafa v <r_venancio@protonmail.com>
2024-02-20 20:17:43 -0300

    Frontend desenvolvido com HTML e CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
 import './App.css';
-import Home from './paginas-home/Home';
-import Navbar from 'f:/Minha pasta/blog-pessoal/src/components/navbar/NavBar';
+
+import Navbar from '../src/components/navbar/NavBar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './paginas-home/login/login';
+import Login from '../src/paginas-home/login/login';
+import Cadastro from '../src/paginas-home/cadastro/cadastro';
+import Home from '../src/paginas-home/Home';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+   
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+  
     </>
-);
+  );
 }
 export default App;
